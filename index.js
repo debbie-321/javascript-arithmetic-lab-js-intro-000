f17bfb8f6038d3eace42b83c5db62a3c39f6c5f9
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-17 11:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function subtract(a, b) {
   return a - b
 }
 
-function multiple(a, b){
+function multiply(a, b){
   return a * b
 }
 
@@ -23,5 +23,9 @@ function decrement(n) {
 }
 
 function makeInt(n){
-  
+  return parseInt('n', 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat('n')
 }
